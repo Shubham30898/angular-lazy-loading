a96f44fb7f3d4917ee10b36d2a5c680a3a77fcbb
shubham <shubham30898@gmail.com>
2023-09-11 23:53:38 +0530

    angular lazy loading with shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component1Component } from '../components/component1/component1.component';
+
+
+@NgModule({
+  declarations: [
+    Component1Component
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    Component1Component
+  ]
+})
+export class SharedModule { }
